666e1bca2a3a8b2e04255748dbceb1769aacb1e3
Voorde, Ted van der (ITOP AT) - KL <Ted-van-der.Voorde+afklm@klm.com>
2026-08-14 14:19:16 +0200

    readme

---
+++ config/generic.tfvars
@@ -0,0 +1,9 @@
+environments = [
+    { name = "hubspoke" },
+    { name = "spoke01" },
+    { name = "spoke02" },
+]
+
+number = 15712567827395
+
+location = "Sweden Central"
--- infra/hubspoke/main.tf
+++ infra/hubspoke/main.tf
@@ -1,12 +1,27 @@
 
-resource "azurerm_resource_group" "hub" {
-  name     = "rg1"
-  location = "West Europe"
+variable environments {
+  type = list(object({
+    name = string
+  }))
 }
 
-resource "azurerm_virtual_network" "hub" {
-  name                = "hub1"
-  location            = azurerm_resource_group.hub.location
-  resource_group_name = azurerm_resource_group.hub.name
-  address_space       = ["10.0.0.0/16"]
+variable number {
+  type = number
 }
+
+variable location {
+  type = string
+}
+
+resource "azurerm_resource_group" "item" {
+  for_each = { for env in var.environments : env.name => env  }
+  name     = "rg${var.number}-${each.value.name}"
+  location = var.location
+}
+
+# resource "azurerm_virtual_network" "hub" {
+#   name                = "hub1"
+#   location            = azurerm_resource_group.hub.location
+#   resource_group_name = azurerm_resource_group.hub.name
+#   address_space       = ["10.0.0.0/16"]
+# }
