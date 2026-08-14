3ec12f251fb64ba8c413c840d1e29b59a442dd32
Voorde, Ted van der (ITOP AT) - KL <Ted-van-der.Voorde+afklm@klm.com>
2026-08-14 17:31:18 +0200

    cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,37 +28,3 @@ echo ARM_SUBSCRIPTION_ID=$(az account show --query id -o tsv)
 echo STORAGE_ACCOUNT_NAME=sa$number
 echo STORAGE_RESOURCE_GROUP=rgstate$number
 echo "number = $number"
-
-# idrgstate=$(az group create -g rgstate$number --location $location --query "id" -o tsv)
-
-# # platform
-
-# idplatform=$(az identity create --name miplatform$number --resource-group rgidentity$number --query "principalId" -o tsv)
-# idplatformclientid=$(az identity show -g rgidentity$number -n miplatform$number --query "clientId" -o tsv)
-# az role assignment create --assignee-object-id $idplatform --role "Contributor" --scope $idrgstate
-# az role assignment create --assignee-object-id $idplatform --role "Reader" --scope $idrgidentity
-# az role assignment create --assignee-object-id $idplatform --role "Owner" --scope $idrgplatform
-
-# # admin
-
-# idadmin=$(az identity create --name miadmin$number --resource-group rgidentity$number --query "principalId" -o tsv)
-# idadminclientid=$(az identity show -g rgidentity$number -n miadmin$number --query "clientId" -o tsv)
-# az role assignment create --assignee-object-id $idadmin --role "Contributor" --scope $idrgplatform
-# az role assignment create --assignee-object-id $idadmin --role "Azure Kubernetes Service RBAC Cluster Admin" --scope $idrgplatform
-# az identity federated-credential create --name "github" --identity-name miadmin$number -g rgidentity$number --issuer "https://token.actions.githubusercontent.com" --subject "repo:tvdvoorde/secureaks:ref:refs/heads/main" --audiences "api://AzureADTokenExchange"
-
-# # developer
-
-# iddeveloper=$(az identity create --name mideveloper$number --resource-group rgidentity$number --query "principalId" -o tsv)
-# iddeveloperclientid=$(az identity show -g rgidentity$number -n mideveloper$number --query "clientId" -o tsv)
-# az role assignment create --assignee-object-id $iddeveloper --role "Reader" --scope $idrgplatform
-# az role assignment create --assignee-object-id $iddeveloper --role "Azure Kubernetes Service RBAC Reader" --scope $idrgplatform
-# az role assignment create --assignee-object-id $iddeveloper --role "Azure Kubernetes Service Cluster User Role" --scope $idrgplatform
-# az identity federated-credential create --name "github" --identity-name mideveloper$number -g rgidentity$number --issuer "https://token.actions.githubusercontent.com" --subject "repo:tvdvoorde/secureaks:ref:refs/heads/main" --audiences "api://AzureADTokenExchange"
-
-# echo "PLATFORM CLENT ID: $idplatformclientid"
-# echo "ADMIN CLENT ID: $idadminclientid"
-# echo "DEVELOPER CLIENT ID:" $iddeveloperclientid
-
-# # az role assignment create --role "Azure Kubernetes Service RBAC Reader" --assignee <AAD-ENTITY-ID> --scope $AKS_ID/namespaces/<namespace-name>
-
