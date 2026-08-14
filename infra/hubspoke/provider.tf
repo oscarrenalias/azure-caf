5785dcd3d8a860b1867bc6fc0d4079e3cc668ff1
Voorde, Ted van der (ITOP AT) - KL <Ted-van-der.Voorde+afklm@klm.com>
2026-08-14 14:02:16 +0200

    test

--- infra/hubspoke/provider.tf
+++ infra/hubspoke/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      #   version = "=3.88.0"
+      version = "4.81.0"
     }
   }
   backend "azurerm" {
@@ -14,4 +14,4 @@ terraform {
 provider "azurerm" {
   features {
   }
-}
+}   
