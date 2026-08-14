ab67527dca72a0d276170a68c79d40d12bd000e5
Voorde, Ted van der (ITOP AT) - KL <Ted-van-der.Voorde+afklm@klm.com>
2026-08-14 14:30:00 +0200

    test

--- config/global.tfvars
+++ config/global.tfvars
@@ -1,7 +1,7 @@
 environments = [
-    { name = "hubspoke", range = "10.0.0.0/16" },
-    { name = "spoke01", range = "10.1.0.0/16" },
-    { name = "spoke02", range = "10.2.0.0/16" },
+  { name = "hubspoke", range = "10.0.0.0/16" },
+  { name = "spoke01", range = "10.1.0.0/16" },
+  { name = "spoke02", range = "10.2.0.0/16" },
 ]
 
 number = 15712567827395
--- infra/hubspoke/main.tf
+++ infra/hubspoke/main.tf
@@ -1,27 +1,28 @@
 
-variable environments {
+variable "environments" {
   type = list(object({
     name = string
   }))
 }
 
-variable number {
+variable "number" {
   type = number
 }
 
-variable location {
+variable "location" {
   type = string
 }
 
 resource "azurerm_resource_group" "item" {
-  for_each = { for env in var.environments : env.name => env  }
+  for_each = { for env in var.environments : env.name => env }
   name     = "rg${var.number}-${each.value.name}"
   location = var.location
 }
 
-# resource "azurerm_virtual_network" "hub" {
-#   name                = "hub1"
-#   location            = azurerm_resource_group.hub.location
-#   resource_group_name = azurerm_resource_group.hub.name
-#   address_space       = ["10.0.0.0/16"]
-# }
+resource "azurerm_virtual_network" "item" {
+  for_each            = { for env in var.environments : env.name => env }
+  name                = "vnet${var.number}-${each.value.name}"
+  location            = azurerm_resource_group.item[each.key].location
+  resource_group_name = azurerm_resource_group.item[each.key].name
+  address_space       = [each.value.range]
+}
